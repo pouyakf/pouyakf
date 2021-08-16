c1343aeb402893bbad95f94849d951a14e8efa27
Pouya <88946732+pouyakf@users.noreply.github.com>
2021-08-16 15:40:02 +0430

    Create KF.py

---
+++ KF.py
@@ -0,0 +1,11 @@
+# z = 187
+# for item in range(1, z + 1):
+#     if z % item == 0:
+#         print(item)
+# #---------------------
+# z = 187
+# i = 1
+# while i < z + 1:
+#     if z % i == 0:
+#         print(i)
+#     i = i + 1
